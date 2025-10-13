310098ddfb1af05d808ac8b3eb43d9b963ad85ca
unknown <mukhlish285@gmail.com>
2025-10-13 20:08:22 +0700

    modified index.js add views/

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const PORT = 8000;
 
 //init app
 const app = express();
 
+const connectionUrl = "mongodb://localhost:27017/todoDB";
+
+mongoose.connect(connectionUrl).then(() => console.log("Database connection succesfull")).catch((error) => console.log(error.message));
+
+
 //view engine
 app.set("view engine", "ejs");
 
+app.get("/",(req, res, next) =>{
+    try{
+        res.render("index");
+    }catch(error){
+        res.status(500).json({message: error.message});
+    }
+});
+
+app.get("/add-todo", (req, res, next) =>{
+    try{
+        res.render("newTodo");
+    }catch(error){
+        res.status(500).json({message: error.message});
+    }
+});
+
+app.get("/update-todo",(req, res, next) =>{
+    try{
+        res.render("updateTodo");
+    }catch(error){
+        res.status(500).json({message: error.message});
+    }
+});
 //listen server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
