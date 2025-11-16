53105ee23f0c04ab7396b6ad9e1ec5e445800cbc
unknown <mukhlish285@gmail.com>
2025-11-16 15:03:41 +0700

    add /add-todo

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
+const bodyParser = require("body-parser")
 
 const PORT = 8000;
 
@@ -10,13 +12,23 @@ const connectionUrl = "mongodb://localhost:27017/todoDB";
 
 mongoose.connect(connectionUrl).then(() => console.log("Database connection succesfull")).catch((error) => console.log(error.message));
 
+const todoSchema = mongoose.Schema({
+    title: {type: String, required: true}, 
+    desc:{type:String}
+}, {timestamps: true});
+
+const Todo = mongoose.model("todo",todoSchema)
+
 
 //view engine
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname,"public")));
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/",(req, res, next) =>{
+app.get("/",async(req, res, next) =>{
     try{
-        res.render("index");
+        const todos = await Todo.find({}).sort({createdAt: -1});
+        res.render("index",{title: "List todo", todos});
     }catch(error){
         res.status(500).json({message: error.message});
     }
@@ -24,7 +36,7 @@ app.get("/",(req, res, next) =>{
 
 app.get("/add-todo", (req, res, next) =>{
     try{
-        res.render("newTodo");
+        res.render("newTodo",{title: "New todo"});
     }catch(error){
         res.status(500).json({message: error.message});
     }
@@ -32,11 +44,36 @@ app.get("/add-todo", (req, res, next) =>{
 
 app.get("/update-todo",(req, res, next) =>{
     try{
-        res.render("updateTodo");
+        res.render("updateTodo",{title: "Update todo"});
     }catch(error){
         res.status(500).json({message: error.message});
     }
 });
+
+app.get("/delete-todo",(req,res, next) =>{
+    try {
+        res.render("deleteTodo",{title: "Delete todo"});
+    } catch (error) {
+        res.status(500).json({message: error.message})
+        
+    }
+})
+
+app.post("/add-todo", async(req,res,next)=>{
+    try {
+        const {title, desc} = req.body;
+
+        if(!title)
+            return res.status(400).json({message: "Title is required"})
+
+        const newTodo = new Todo({title,desc});
+        await newTodo.save()
+
+        res.redirect("/")
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
 //listen server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
